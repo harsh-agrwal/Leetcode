007b5d87346cf243bc0de7cca586a5cea784fe35
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-04-10 10:18:02 +0530

    Create First Missing Positive.java

---
+++ First Missing Positive.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i,flag=0,out=0;
+        Arrays.sort(nums);
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        return 1;
+        else
+        {
+            for(i=0;i<nums.length-1;i++)
+            {
+                if(nums[i]>0)
+                {
+                    if(nums[i+1]==nums[i]+1)
+                    continue;
+                    else if(nums[i]==nums[i+1])
+                    continue;
+                    else
+                    return nums[i]+1;
+                }
+            }
+        }return nums[i]+1;
+        
+    }
+}
